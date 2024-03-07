18a766960e5d576ae6b76d83b3d6df053b302c2a
Rich Snapp <rich.snapp@gmail.com>
2024-03-07 13:42:00 -0700

    terraform tutorials

---
+++ learn-terraform/docker/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0.1"
+    }
+  }
+}
+
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = false
+}
+
+resource "docker_container" "nginx" {
+  image    = docker_image.nginx.image_id
+  name     = var.container_name
+
+  ports {
+    internal = 80
+    external = 8080
+  }
+}
---
+++ learn-terraform/gcp/terraform.tfvars
@@ -0,0 +1,2 @@
+project                  = "richsnapp-174618"
+credentials_file         = "richsnapp-174618-0be086f8a55f.json"
